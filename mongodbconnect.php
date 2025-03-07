d50fe2e1079dbb0e46c5b65c96fb1879bb2ddf1a
c22emigr <c22emigr@student.his.se>
2025-03-07 12:00:05 +0100

    Början på php-applikation med MongoDB-koppling

--- mongodbconnect.php
+++ mongodbconnect.php
@@ -1,9 +1,58 @@
 <?php
-try {
-$conn = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-} catch (MongoDBDriverExceptionException $e) {
-echo 'Failed connection!<br /><br />';
-echo $e->getMessage();
-exit();
+require 'vendor/autoload.php';
+
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$collection = $client->mydatabase->users;
+
+$searchResult = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $searchResult = $collection->find(['name' => $name]);
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Invest0iQ</title>
+</head>
+
+<body>
+    <div>
+        <img src="" alt="" class="svg">    
+    </div>
+
+    <div class="centrera">
+        <h1>Invest0iQ</h1>
+    </div>
+    
+    <div>
+        <form method="POST" class="centrera">
+            <div>
+                <h2>Aktiesök</h2>
+
+                <div class="form">
+                    <input type="text" name="" id="" placeholder="Aktiesök">
+                    <button type="submit" class="" id="" onclick=""></button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div>
+        <ul>
+            <?php foreach ($searchResult as $doc): ?>
+                <li><?php echo "Namn: " . $doc['name'] . " - Email: " . $doc['email']; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
+</body>
+</html>
