c10d09eda10d1de19c3702e5e6b950839b0b31b6
c22emigr <c22emigr@student.his.se>
2025-03-21 14:21:25 +0100

    HTML och PHP för MariaDB

--- mariadbconnect.php
+++ mariadbconnect.php
@@ -8,3 +8,68 @@ if($conn->connect_error){
 die("Connection failed".$conn->connect_error);
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Invest0iQ</title>
+</head>
+
+<body>
+    <div>
+        <img src="" alt="" class="svg">    
+    </div>
+
+    <div class="centrera">
+        <h1>Invest0iQ</h1>
+    </div>
+    
+    <div>
+        <form method="POST" class="centrera">
+            <div>
+                <h2>Browse stocks</h2>
+
+                <div class="form">
+                    <input type="text" name="name" id="stocksearch" placeholder="Stock Search">
+                    <button type="submit" class="" id="stocksearchbutton" onclick="">Search</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="centrera">
+        <table>
+        <thead>
+            <tr class="pad">
+                <th>Stock name</th>
+                <th>Datetime</th>
+                <th>Open</th>
+                <th>High</th>
+                <th>Low</th>
+                <th>Close</th>
+                <th>Volume</th>
+                <th>Value</th>
+            </tr>
+        </thead>
+                <?php foreach ($documents as $doc): ?>
+                    <tr class="pad">
+                        <td><?php echo $doc['stock_name']; ?></td>
+                        <td><?php echo $doc['datetime']; ?></td>
+                        <td><?php echo $doc['open']; ?></td>
+                        <td><?php echo $doc['high']; ?></td>
+                        <td><?php echo $doc['low']; ?></td>
+                        <td><?php echo $doc['close']; ?></td>
+                        <td><?php echo $doc['volume']; ?></td>
+                    </tr>                
+                <?php endforeach; ?>
+        </table>
+    </div>
+
+</body>
+</html>
--- mätscript.js
+++ mätscript.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name         Mätscript
+// @name         Mätscript Examensarbete
 // @namespace    http://tampermonkey.net/
-// @version      2024-09-24
-// @description  Script för att mäta söktiden för ord i artiklar
+// @version      2025-05-18
+// @description  Script för att mäta söktiden för aktier i en aktieapplikation
 // @author       You
-// @match        http://grupp1.cms.webug.se/
-// @include      http://grupp1.cms.webug.se/
+// @match        http://localhost/examensarbete/mongodbconnect.php
+// @include      http://localhost/examensarbete/mongodbconnect.php
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
 
-var noun=new Array("bedroom","song","structure","pickle","nut","plane","room","dust","carpenter","camp","office","grape","fuel","cap","minister","pest","morning","scarecrow","trip","test","stream","month","summer","tub","baseball","cast","dime","arm","rake","money","hen","straw","head","cobweb","geese","cat","glove","edge","actor","daughter","space","substance","flower","self","sound","fang","fight","toe","fork","kitten","rabbit","bean","swing","oranges","mitten","fog","class","nose","pleasure","flesh","feather","river","celery","van","scale","badge","robin","sweater","gate","mountain","alarm","pollution","cake","donkey","wealth","oatmeal","cherry","joke","faucet","summer","airplane","reward","scissors","believe","rain","lettuce","zipper","club","songs","kite","kiss","kick","sea","grandmother","jam","moon","veil","juice","stomach","frame","hill","fruit","queen","boundary","pig","drug","giraffe","shape","limit","fiction","loaf","face","toothache","tramp","rate","railway","sponge","flag","hope","education","beginner","boy","creature","notebook","voice","meeting","bird","coach","team","hose","riddle","linen","wrist","grim reaper","mint","plantation","fifth","quicksand","library","family","cat","brick","value","help","recess","parent","soda","icicle","son","stove","street","wing","flock","deer","winter","sea","railway","sack","mongrel","pan","pail","show","apparel","governor","ladybug","wool","lift","weather","afterthought","achieve","meal","hook","note","step","acoustics","shop","water","grandmother","umbrella","grandfather","man","island","hope","zoo","history","day","pie","line","volleyball","beam","anger","cellar","interest","rat","owner","holiday","order","faucet","pot","swim","chin","sweater","celery","sugar","lunch","space","road","wilderness","garden","Steve Jobs","linen","pencil","feet","meal","name","sun","trail","passenger","rifle","arithmetic","toothpaste","screw","milk","volcano","hen","able","ball","rock","cabbage","hobbies","judge","friends","cellar","zebra","spiders","opinion","cub","front","grain","foot","town","surprise","bed","offer","furniture","hairspray","bead","sofa","fog","magic","spade","suit","cattle","hall","nest","station","ghost","brother","week","knot","haircut","vein","beggar","beast","bushes","galley","vegetable","year","eye","balloon","pear","underwear","book","Ipad","income","thread","cars","secretary","hill","zephyr","glue","lawyer","horn","death","brush","hole","water","approval","view","kitty","stream","smoke","sister","wish","underwear","thrill","reaction","metal","girls","grandfather","appliance","language","fold","liquid","grain","food","soup","mother","battle","flower","stranger","hydrant","soda","children","moon","carpenter","stench","owl","veil","book","hate","corn","ache","government","gate","feast","crate","lettuce","plot","wrench","caption","clover","cause","animal","force","furniture","volcano","wheel","farm","friction","crowd","pail","grade","argument","throne","number","slave","elbow","governor","route","sidewalk","stranger","kite","crayon","education","reason","carriage","servant","morning","glass","answer","tent","straw","beetle","heart","horn","father","dirt","food","playground","beef","advice","sheet","jellyfish","throat","motion","aftermath","bread","apple","cream","police","scent","texture","map","cactus","breakfast","popcorn","visitor","suit","patch","doctor","afternoon","sink","lumber","bath","lamp","dad","sail","great uncle","cats","arch","effect","kettle","cart","measure","cave","kiss","cemetery","spark","volleyball","game","story","ocean","plant","good-bye","oven","writer","insurance","yard","seed","range","circle","sock","horse","frog","game","table","digestion","trick","oatmeal","title","meat","bun","unit","swing","cable","dust","snake","giants","cannon","sofa","health","notebook","monkey","system","cracker","arithmetic","feeling","font","holiday","basket","dress","cent","stocking","goldfish","can","girl","brain","oil","tank","key","rainstorm","instrument","apple","home","jar","bike","invention","development","calculator","heat","string","action","channel","jeans","string","spark","frame","rainstorm","lizard","hearing","territory","dog","coil","jail","zinc","stone","level","girl","scene","soap","butter","spy","bomb","light","guide","mom","toes","doll","selection","cow","vase","earthquake","hot","elbow","car","pet","rain","fear","stew","lightning bolt","mailbox","receipt","bucket","vest","dock","park","bubble","geese","wood","niece","finger","goose","cattle","turkey","seat","fuel","lunchroom","stove","seashore","lake","hobbies","hook","pear","birthday","shoe","spoon","temper","boot","banana","vacation","tiger","knife","summer","snow","winter","throne","squirrel","crown","bat","fowl","ray","song","form","treatment","owl","goldfish","farmer","friction","knowledge","toothbrush","stage","goose","feather","desk","lace","stretch","crook","heat","son","giraffe","flavor","mother","clam","earthquake","airport","downtown","sense","plastic","aunt","van","chicken","operation","friend","organization","flowers","mice","music","basketball","cloth","creator","voyage","branch","hair","rat","orange","pancake","umbrella","laborer","heart","partner","scarf","sun","wren","list","fly","calendar","cup","marble","twig","home","coast","grade","eggnog","dinner","skate","field","activity","feet","seashore","boy","bait","kite","locket","seed","insect","maid","mountain","scale","tree","ornament","cemetery","guitar","rake","crow","frog","spot","rose","support","health","honey","society","crib","army","light","sort","north","poison","border","street","quiet","tray","drum","pinkie","month","vest","quilt","science","recess","cast","vegetable","vase","use","feast","picture","rail","honey","lock","pocket","hat","reading","pen","ghost","toad","uncle","memory","star","lip","fang","grass","voyage","gun","visitor","fan","knee","idea","goose","stealth yacht","cave","mask","sleet","number","snail","observation","ocean","fireman","mother");
+// Lista över sökord
+var noun = ["", "Agilent Technologies, Inc. (A)", "American Airlines Group Inc", "Apple Inc", "AbbVie Inc", "Airbnb Inc", "Abbott Laboratories", "Arch Capital Group Ltd", "Accenture Plc", "Adobe Inc", "Analog Devices Inc", "Archer-Daniels-Midland Co", "Automatic Data Processing Inc", "Autodesk Inc", "Ameren Corp", "American Electric Power Company Inc", "AES Corp", "Aflac Inc", "American International Group Inc", "Assurant Inc", "Arthur J. Gallagher & Co.", "Akamai Technologies, Inc.", "Albemarle Corp", "Align Technology, Inc.", "Allstate Corp", "Allegion PLC", "Applied Materials Inc", "Amcor PLC", "Advanced Micro Devices Inc", "Ametek Inc", "Amgen Inc", "Ameriprise Financial, Inc.", "American Tower Corp", "Amazon.com Inc", "Arista Networks Inc", "ANSYS Inc", "Aon PLC", "A O Smith Corp", "APA Corp (US)", "Air Products and Chemicals Inc", "Amphenol Corp", "Aptiv PLC", "Alexandria Real Estate Equities Inc", "ASML Holding NV", "Atmos Energy Corp", "AvalonBay Communities Inc", "Broadcom Inc", "Avery Dennison Corp", "American Water Works Company Inc", "Axon Enterprise Inc", "American Express Co", "AstraZeneca PLC", "Autozone Inc", "Boeing Co", "Bank of America Corp", "Ball Corp", "Baxter International Inc", "Bath & Body Works Inc", "Best Buy Co Inc", "Becton Dickinson and Co", "Franklin Resources Inc", "Brown-Forman Corp Class B", "Bunge Global SA", "Biogen Inc", "Bio-Rad Laboratories, Inc. Class A Common Stock", "Bank of New York Mellon Corp", "Booking Holdings Inc", "Baker Hughes Co", "Builders FirstSource, Inc.", "BlackRock Inc", "Bristol-Myers Squibb Co", "Broadridge Financial Solutions Inc", "Berkshire Hathaway Inc Class B", "Brown & Brown Inc", "Boston Scientific Corp", "BorgWarner Inc", "Blackstone Inc", "BXP Inc", "Citigroup Inc", "Conagra Brands Inc", "Cardinal Health Inc", "Carrier Global Corp", "Caterpillar Inc", "Chubb Ltd", "Cboe Global Markets Inc", "CBRE Group Inc", "Coca-Cola Europacific Partners PLC", "Crown Castle Inc", "State Street Corp", "Seagate Technology Holdings PLC", "Constellation Brands Inc", "Stanley Black & Decker Inc", "Skyworks Solutions Inc", "Synchrony Financial", "Stryker Corp", "Sysco Corp", "AT&T Inc", "Molson Coors Beverage Co Class B", "TransDigm Group Inc", "Teledyne Technologies Inc", "Atlassian Corp", "BIO-TECHNE Corp", "TE Connectivity PLC", "Teradyne Inc", "Truist Financial Corp", "Teleflex Inc", "Target Corp", "TJX Companies Inc", "Thermo Fisher Scientific Inc", "T-Mobile Us Inc", "Tapestry Inc", "Targa Resources Corp", "Trimble Inc", "T Rowe Price Group Inc", "Travelers Companies Inc", "Tractor Supply Co", "Tesla Inc", "Tyson Foods Inc", "Trane Technologies PLC", "Trade Desk Inc", "TAKE-TWO INTERACTIVE SOFTWARE, INC Common Stock", "Texas Instruments Inc", "Textron Inc", "Tyler Technologies Inc", "United Airlines Holdings Inc", "Uber Technologies Inc", "UDR Inc", "Universal Health Services Inc", "Ulta Beauty, Inc.", "UnitedHealth Group Inc", "Union Pacific Corp", "United Parcel Service Inc", "United Rentals, Inc.", "US Bancorp", "Visa Inc", "VF Corp", "VICI Properties Inc", "Valero Energy Corporation", "Vulcan Materials Company", "Verisk Analytics, Inc.", "VeriSign, Inc", "Vertex Pharmaceuticals Inc", "Ventas Inc", "Viatris Inc", "Verizon Communications Inc", "Westinghouse Air Brake Technologies Corp", "Waters Corp", "Walgreens Boots Alliance Inc", "Warner Bros Discovery Inc", "Workday Inc", "Western Digital Corp", "WEC Energy Group Inc", "Welltower Inc", "Wells Fargo & Co", "Whirlpool Corp", "Waste Management Inc", "Williams Companies Inc", "Walmart Inc", "W R Berkley Corp", "WRKR Ltd", "West Pharmaceutical Services Inc", "Willis Towers Watson PLC", "Weyerhaeuser Co", "Wynn Resorts, Limited", "Xcel Energy Inc", "Exxon Mobil Corp", "DENTSPLY SIRONA Inc", "Xylem Inc", "Yum! Brands Inc", "Zimmer Biomet Holdings Inc", "Zebra Technologies Corp", "Zions Bancorporation NA", "Zscaler Inc", "Zoetis Inc"];
 
-// Main-funktionen som generarar randomtexten
 // KOD SOM SKAPAR SEED
 function jsf32(a, b, c, d) {
   a |= 0; b |= 0; c |= 0; d |= 0;
@@ -59,7 +59,7 @@ Math.randSeed = Math.floor(Date.now());
             Math.setSeed(counter);
             var search = noun[Math.floor(Math.random()*noun.length)];
 
-            var searchField = document.getElementById('mod-finder-searchword110'); // Sök på noun från ordlistan
+            var searchField = document.getElementById('stocksearch'); // Sök på noun från ordlistan
             if (searchField) {
                 searchField.value = search;
 
@@ -69,7 +69,7 @@ Math.randSeed = Math.floor(Date.now());
 
             // Skicka formuläret och klicka på "Spara & Stäng" automatiskt efter en delay
             setTimeout(function() {
-                var form = document.querySelector(".mod-finder.js-finder-searchform.form-search");
+                var form = document.querySelector("stocksearchbutton");
                 var measurement = performance.now();
                 var old = parseInt(localStorage.getItem("oldVal")) || 0;
                 if (form) {
@@ -80,6 +80,61 @@ Math.randSeed = Math.floor(Date.now());
             }); // 500-millisekunders fördröjning
         }else{
          // localStorage.removeItem("counter");
+        }
+    });
+})();
+
+
+// Funktion som körs efter sökning
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() { // Väntar på att sidan laddat klart
+        var measurement = performance.now(); // tiden mäts
+        var old = parseInt(localStorage.getItem("oldVal")) || 0;
+        localStorage.setItem("oldVal", measurement);
+        var delta = Math.round(measurement - old);
+
+        var search = localStorage.getItem("search");
+        var str = localStorage.getItem("theData") || '';
+        str += delta + ',' + search + "\n"; // Delta är skillnaden dvs söktiden samt sökordet är med
+        localStorage.setItem("theData", str);
+
+       // alert(str); // Temporär alert för att slippa ladda ned
+
+        var counter = parseInt(localStorage.getItem("counter")); // Hämtar counter
+        var globalCounter = parseInt(localStorage.getItem("globalCounter")) || 0; // Global counter / andra räknaren
+
+
+        if (counter === 75){
+            globalCounter++; // Increment the global counter
+            localStorage.setItem("globalCounter", globalCounter); // Global counter sparas
+            localStorage.setItem("counter", 0); // Vanliga countern börjar om efter 10
+        }
+        // Kör X-antal mätningar innan nedladdning
+        if (globalCounter === 40) { // Justera siffran här för att välja hur många sökningar som sker innan nedladdning
+
+            // Provade konvertera till blob för att få nedladdning att fungera
+            var blob = new Blob([str], { type: "text/csv;charset=utf-8;" });
+            var link = document.createElement("a");
+            var url = URL.createObjectURL(blob);
+
+            // Skapar länk för nedladdningen
+            link.setAttribute("href", url);
+            link.setAttribute("download", "measurement_aktiedata.csv");
+            document.body.appendChild(link);
+
+            // Laddar ner
+            link.click();
+
+            // Tar bort den osynliga länken efter nedladdning
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+
+            localStorage.setItem("globalCounter", 0); // Globala countern resettas efter nedladdning skett
+            localStorage.removeItem("theData"); // Den gamla datan tas bort så att filen bara har de senaste mätningarna
+        }else{ // Om nedladdning inte sker tas man tillbaka
+        window.location.href = "http://localhost/examensarbete/mongodbconnect.php"; // Går tillbaka till föregående sida efter mätning
         }
     });
 })();
