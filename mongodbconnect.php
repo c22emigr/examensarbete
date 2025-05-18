c1a1f15a993febf2876836b4a21427f82500a783
c22emigr <c22emigr@student.his.se>
2025-05-18 16:09:06 +0200

    50

--- mongodbconnect.php
+++ mongodbconnect.php
@@ -2,7 +2,7 @@
 require '../vendor/autoload.php';
 
 $client = new MongoDB\Client("mongodb://localhost:27017");
-$collection = $client->examensarbete->aktier75;
+$collection = $client->examensarbete->aktier50;
 
 $bucket = $client->examensarbete->selectGridFsBucket();
 
